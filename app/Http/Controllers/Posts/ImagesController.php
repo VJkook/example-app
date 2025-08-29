6771e01fc92e8620e3546ae7e229ff5255e3568d
marina <marinasishkina@gmail.com>
2025-08-29 07:53:01 +0300

    добавлены post для создания и обновления картинки текстом (дкартинка не отображается)

--- app/Http/Controllers/Posts/ImagesController.php
+++ app/Http/Controllers/Posts/ImagesController.php
@@ -15,5 +15,58 @@ class ImagesController extends Controller
         return response()->json($image);
     }
 
+    public function show($id)
+    {
+        $image = Image::find($id);
+        if (!$image) {
+            return response("Image not found", 404);
+        }
+        return response()->json($image);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validate = $request->validate([
+                'url' => "required|string",
+            ]);
+
+            $image = Image::create($validate);
+            return response()->json([
+                "message" => "Image created",
+                "data" => $image
+            ], 201);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Обработка ошибок валидации
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422); // 422 - Unprocessable Entity
+
+        } catch (\Exception $e) {
+            // Обработка всех остальных ошибок
+            return response()->json([
+                'message' => 'Server error',
+                'error' => $e->getMessage()
+            ], 500); // 500 - Internal Server Error
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validate = $request->validate([
+            'url' => "required|string",
+        ]);
+
+        $image = Image::find($id);
+        if (!$image) {
+            return response("Image not found", 404);
+        }
+        $image->update($validate);
+        return response()->json([
+            "message" => "Image updated",
+            "data" => $image,
+        ], 200);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,10 @@ Route::withoutMiddleware(['web', 'csrf'])->group(function () {
     Route::get('/posts/{id}', [PostsController::class, 'show']);
     Route::post('/posts', [PostsController::class, 'store']);
     Route::post('posts/{id}',[PostsController::class, 'update']);
+
     Route::get('/images', [ImagesController::class, 'index']);
+    Route::get('/images/{id}', [ImagesController::class, 'show']);
+    Route::post('/images', [ImagesController::class, 'store']);
+    Route::post('/images/{id}', [ImagesController::class, 'update']);
 });
 
